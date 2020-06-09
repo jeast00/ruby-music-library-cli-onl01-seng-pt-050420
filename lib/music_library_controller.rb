0cd175c0afadfc81b68e902f734a9057c33e2cc4
Jonathan East <jonathan_east@ymail.com>
2020-06-09 01:03:06 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -46,7 +46,11 @@ class MusicLibraryController
   end
 
   def list_songs
-    Song.all.sort{ |song_a, song_b| song_a.name <=> song_b.name }.map.with_index {|song, index| puts "#{index+1}. " + song.artist.name + " - " + song.name + " - " + song.genre.name }
+    Song.all.sort{ |song_a, song_b|
+      song_a.name <=> song_b.name
+    }.map.with_index {|song, index|
+      puts "#{index+1}. " + song.artist.name + " - " + song.name + " - " + song.genre.name
+    }
   end
 
 end
