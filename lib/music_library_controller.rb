001ad8efb4a25d48fe21932a6665f76673ead321
Jonathan East <jonathan_east@ymail.com>
2020-06-09 00:57:43 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -46,7 +46,7 @@ class MusicLibraryController
   end
 
   def list_songs
-
+    Song.all.sort.each_with_index {|song, index| puts "#{index+1}. " + song.artist.name + " - " + song.name + " - " + song.genre.name }
   end
 
 end
